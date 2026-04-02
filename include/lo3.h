e1d8263ba3d5a38090cf168eac5f1069e28bf791
seesee010 <sebastian.daubner2@gmail.com>
2026-04-02 18:10:29 +0200

    now every plugin justs need to use lo3.h

--- include/lo3.h
+++ include/lo3.h
@@ -2,12 +2,92 @@
 // see License file for more informations
 
 /*
- * For all other plugins
+ * For all other plugins!
+ */
+
+// important:
+
+/*
+ * Use this as your header for any plugin you create.
+ * this header file already uses everything YOU need to have.
  */
 
 #pragma once
 
-#include "../src/internal/additional-naming.h"
-#include "../src/internal/bare-define.h"
-#include "../src/internal/bare-var.h"
-#include "../src/internal/global.h"
+///// bare-define.h
+typedef enum {
+
+	TYPE_num = '$',
+	TYPE_string = '_',
+	TYPE_array = '*',
+	TYPE_var = '%'
+} lo3_types;
+
+typedef union {
+	int num;
+	char *string;
+} lo3_value;
+
+typedef struct {
+
+	lo3_types type;
+	int chooseType; // 0(=num), 3(=string), everything else(=invalid!)
+
+	lo3_value value;
+
+} lo3_val;
+
+///// additional-naming.h /////
+
+// just some values for the exec,
+// to not use some random values.
+#define ATYPE_num 0b00
+#define ATYPE_var 0b01
+#define ATYPE_array 0b10
+#define ATYPE_string 0b11
+
+// sleep(ms) macro
+#ifdef _WIN32
+#inlcude < windows.h>
+#define ASLEEP(ms) Sleep(ms);
+
+#else // linux
+#include <unistd.h>
+#define ASLEEP(ms) slee((ms) * 1000) // mic sec -> ms
+
+#endif
+
+///// bare-var.h /////
+#pragma once
+
+// #include "specific-language.h"
+
+typedef struct lo3_var lo3_var;
+
+typedef struct lo3_varList lo3_varList;
+
+///// from var.c /////
+void var_create(const char *name, int type);
+void var_free(const char *name);
+int var_find(const char *name);
+
+lo3_var *var_get(const char *name);
+int var_getType(const lo3_var *var);
+lo3_value var_getValue(const lo3_var *var);
+int var_getNum(const lo3_var *var);
+const char *var_getString(const lo3_var *var);
+
+void var_setNum(const char *name, int num);
+void var_setString(const char *name, char *string);
+
+///// global.h /////
+
+// #include "bare-define.h"
+
+#define G_SIZE 100
+typedef struct lo3_g lo3_g;
+
+// getter and setter
+lo3_val g_get(int index);
+void g_set(int index, lo3_val value);
+int g_isSet(int index);
--- src/internal/bare-var.h
+++ src/internal/bare-var.h
@@ -1,1 +1,24 @@
+// Copyright (c) 2026 seesee010
+// Read the License file for more informations about the license.
+
+#pragma once
+
+#include "specific-language.h"
+
+typedef struct lo3_var lo3_var;
+
+typedef struct lo3_varList lo3_varList;
+
 ///// from var.c /////
+void var_create(const char *name, int type);
+void var_free(const char *name);
+int var_find(const char *name);
+
+lo3_var *var_get(const char *name);
+int var_getType(const lo3_var *var);
+lo3_value var_getValue(const lo3_var *var);
+int var_getNum(const lo3_var *var);
+const char *var_getString(const lo3_var *var);
+
+void var_setNum(const char *name, int num);
+void var_setString(const char *name, char *string);
